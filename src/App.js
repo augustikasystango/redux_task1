1ef56dbd60b7be671a319a17b335e120582a3509
Augustika Pal <augustika.pal@systango.com>
2025-04-10 14:12:09 +0530

    counter using rtk

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Counter from './components/Counter';
+
+
+function App() {
+  return (
+    <>
+    <Counter />
+    <Header/>
+    <Auth/>
+    </>
+    
+    
+  );
+}
+
+export default App;
---
+++ src/components/Counter.js
@@ -0,0 +1,42 @@
+import classes from './Counter.module.css';
+import {useSelector,useDispatch} from 'react-redux';
+import { counterActions } from '../store';
+
+
+const Counter = () => {
+  const dispatch = useDispatch();
+  const counter = useSelector((state)=>state.counter);
+  const show = useSelector((state)=>state.showCounter);
+
+  const incrementHandler=()=>{
+    dispatch(counterActions.increment())
+
+  } 
+
+  const decrementHandler=()=>{
+    dispatch(counterActions.decrement())
+  }
+
+  const increaseHandler=()=>{
+    dispatch(counterActions.increase(10)); //
+  }
+
+  const toggleCounterHandler = ( ) => {
+    dispatch(counterActions.toggle())
+  };
+
+  return (
+    <main className={classes.counter}>
+      <h1>Redux Counter</h1>
+     { show && <div className={classes.value}>{counter}</div>}
+      <button onClick={incrementHandler}>Increment</button>
+     <button onClick={increaseHandler}>
+      Increase by 10
+     </button>
+     <button onClick={decrementHandler}>Decrement</button>
+      <button onClick={toggleCounterHandler}>Toggle Counter</button>
+    </main>
+  );
+};
+
+export default Counter;
